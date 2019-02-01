eff10e2b5f0e6fe505d45fc9a385492915d4add5
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-02-01 09:49:21 -0200

    Update controller.php

--- controller.php
+++ controller.php
@@ -2,24 +2,15 @@
 //Criado por Anderson Ismael
 //21 de janeiro de 2019
 
-function controller($names,$data=false){
-    if(is_array($names)){
-        foreach($names as $name){
-            openController($name,$data);
-        }
-    }else{
-        openController($names,$data);
-    }
-}
-
-function openController($name,$data=false){
+function controller($name,$data=false){
     $str=ROOT.'controller/'.$name.'.php';
     if(file_exists($str)){
         if(is_array($data)){
             extract($data);
         }
         require_once($str);
+        return true;
     }else{
-        die('<b>Erro:</b><br>'.PHP_EOL.'<br>Controller <b>'.$name.'</b> não encontrado'.PHP_EOL);
+        return false;
     }
 }
