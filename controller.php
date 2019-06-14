3f7397845d7ff4b617ff6baadd9a14d9fa1092d5
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-06-14 15:50:54 -0300

    M  controller.php

--- controller.php
+++ controller.php
@@ -7,7 +7,7 @@ function controller($name,$data=false){
     $name2=explode('/',$name);
     $repoName=@$name2[0];
     $nameControllerName=@$name2[1];
-    $str2=ROOT.'/controller/'.$nameControllerName.'.php';
+    $str2=ROOT.$repoName.'/controller/'.$nameControllerName.'.php';
     if(file_exists($str)){
         if(is_array($data)){
             extract($data);
